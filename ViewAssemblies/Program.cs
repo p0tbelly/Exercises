d2142a426765b2226e62266498d3a6fb1173d7ec
p0tbelly <mark.yearsley@gmail.com>
2019-12-15 22:18:17 +0000

    Initial check-in of module ViewAssemblies

---
+++ ViewAssemblies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ViewAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
