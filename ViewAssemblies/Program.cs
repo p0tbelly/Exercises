6b41862531be3e1326dd9251dc966fb8c516fef1
p0tbelly <mark.yearsley@gmail.com>
2019-12-15 22:32:42 +0000

    update

--- ViewAssemblies/Program.cs
+++ ViewAssemblies/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+
 
 namespace ViewAssemblies
 {
@@ -6,7 +9,23 @@ namespace ViewAssemblies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                int methodCount = 0;
+
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+
+                Console.WriteLine(
+                    "{0:N0} types with {1:N0} methods in {2} assembly.",
+                    arg0: a.DefinedTypes.Count(),
+                    arg1: methodCount,
+                    arg2: r.Name);
+            }
         }
     }
 }
