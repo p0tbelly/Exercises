b54111536eb9322061a979851865c947d2f8c921
MARK YEARSLEY <markyearsley@MARKs-MacBook-Pro.local>
2019-12-16 00:25:59 +0000

    Reorganising

---
+++ ViewAssemblies/ViewAssemblies/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+
+namespace ViewAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                int methodCount = 0;
+
+                foreach (var t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Count();
+                }
+
+                Console.WriteLine(
+                    "{0:N0} types with {1:N0} methods in {2} assembly.",
+                    arg0: a.DefinedTypes.Count(),
+                    arg1: methodCount,
+                    arg2: r.Name);
+            }
+        }
+    }
+}
